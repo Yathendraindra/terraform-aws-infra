f4870decdd7bb61b25d45fa855f0e35a201924ee
Yathendraindra <yathendra1997@gmail.com>
2025-10-13 12:34:44 +0530

    Add public and private subnets

--- main.tf
+++ main.tf
@@ -12,3 +12,34 @@ resource "aws_vpc" "main" {
     Project = "terraform-aws-infra"
   }
 }
+
+# -------------------------------
+# Public Subnets
+# -------------------------------
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-subnet-1a"
+    Project = "terraform-aws-infra"
+    Type    = "public"
+  }
+}
+
+# -------------------------------
+# Private Subnets
+# -------------------------------
+resource "aws_subnet" "private_1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name    = "private-subnet-1b"
+    Project = "terraform-aws-infra"
+    Type    = "private"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,12 @@ output "vpc_id" {
   description = "The ID of the created VPC"
   value       = aws_vpc.main.id
 }
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public_1a.id
+}
+
+output "private_subnet_id" {
+  description = "ID of the private subnet"
+  value       = aws_subnet.private_1b.id
+}
